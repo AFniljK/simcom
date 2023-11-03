9f209e09e3f17b6e8beab3984502958e07684387
AFniljK <onlyaforapple@gmail.com>
2023-11-03 11:36:11 +0545

    test sending info to server

--- client/sender/src/main.rs
+++ client/sender/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::Write,
+    net::{SocketAddr, TcpStream},
+};
+
+fn main() -> anyhow::Result<()> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let mut stream = TcpStream::connect(addr)?;
+    let buf = "Hello World!";
+    stream.write_all(buf.as_bytes())?;
+    Ok(())
 }
