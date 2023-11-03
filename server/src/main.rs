9f2680b1ab3de69095792803e7249d32d62aa512
AFniljK <onlyaforapple@gmail.com>
2023-11-03 11:31:57 +0545

    listening/testing io between client and server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::Read,
+    net::{SocketAddr, TcpListener, TcpStream},
+};
+
+use anyhow::Ok;
+
+fn main() -> anyhow::Result<()> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr)?;
+    for stream in listener.incoming() {
+        handle_stream(&mut stream?)?;
+    }
+    Ok(())
+}
+
+fn handle_stream(stream: &mut TcpStream) -> anyhow::Result<()> {
+    let mut buf = String::new();
+    stream.read_to_string(&mut buf)?;
+    println!("{buf:?}");
+    Ok(())
 }
