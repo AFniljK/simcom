e4e01ee96032e8207d927bff6df0e388d940d010
AFniljK <onlyaforapple@gmail.com>
2023-11-03 22:02:50 +0545

    sent sample_rate and built supported_config with fixed buffersize

--- client/sender/src/main.rs
+++ client/sender/src/main.rs
@@ -1,15 +1,38 @@
 use std::net::{SocketAddr, UdpSocket};
 
+use anyhow::anyhow;
+use cpal::{
+    traits::{DeviceTrait, HostTrait},
+    BufferSize,
+};
+
 const INITIAL_BUFFER: &str = "sender";
 
 fn main() -> anyhow::Result<()> {
     let server_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let addr = SocketAddr::from(([127, 0, 0, 1], 0));
+    let host = cpal::default_host();
+    let microphone = host
+        .default_input_device()
+        .expect("cannot find input device");
+    println!("Microphone: {}", microphone.name()?);
 
     let socket = UdpSocket::bind(addr)?;
-    socket.send_to(INITIAL_BUFFER.as_bytes(), server_addr)?;
+    socket.connect(server_addr)?;
+    socket.send(INITIAL_BUFFER.as_bytes())?;
+
     let mut buf = [0; 1024];
-    let (bytes_read, _) = socket.recv_from(&mut buf)?;
-    println!("{:?}", String::from_utf8(buf[0..bytes_read].to_vec()));
+    let bytes_read = socket.recv(&mut buf)?;
+    let response = String::from_utf8(buf[0..bytes_read].to_vec())?;
+    if response == "occupied" {
+        return Err(anyhow!(response));
+    }
+
+    let mut config = microphone.default_input_config()?.config();
+    config.channels = 1;
+    config.buffer_size = BufferSize::Fixed(1024);
+    println!("Config: {config:?}");
+    socket.send(config.sample_rate.0.to_string().as_bytes())?;
+
     Ok(())
 }
