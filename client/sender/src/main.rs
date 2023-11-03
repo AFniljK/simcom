174377a54169846b2c6580ccca62ae8e42ef35c0
AFniljK <onlyaforapple@gmail.com>
2023-11-03 13:06:46 +0545

    reaching the server

--- client/sender/src/main.rs
+++ client/sender/src/main.rs
@@ -3,10 +3,11 @@ use std::{
     net::{SocketAddr, TcpStream},
 };
 
+const INITIAL_BUFFER: &str = "sender";
+
 fn main() -> anyhow::Result<()> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let mut stream = TcpStream::connect(addr)?;
-    let buf = "Hello World!";
-    stream.write_all(buf.as_bytes())?;
+    stream.write_all(INITIAL_BUFFER.as_bytes())?;
     Ok(())
 }
